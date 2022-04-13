926803f7af874a71cb84ac0e870476a855d79af8
ankitsanodiya1 <>
2022-04-13 08:49:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let input;
+input=1;
+switch(input){
+    case 1:
+    console.log("continue....")
+    break;
+    case"y":
+    console.log("continue....")
+    break;
+    case"yes":
+    console.log("continue....")
+    break;
+    case 0:
+    console.log("end....")
+    break;
+    case"n":
+    console.log("end....")
+    break;
+    case"no":
+    console.log("end....")
+    break;
+  default:
+    console.log("wrong input")
+}
